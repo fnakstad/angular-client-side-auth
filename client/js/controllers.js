082aa492dd45fd882c48d82f4353f47629832526
Frederik Nakstad <nakstad@gmail.com>
2013-07-04 15:24:43 +0200

    Change databinding expression on checkboxes

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -52,7 +52,8 @@ angular.module('angular-client-side-auth')
 angular.module('angular-client-side-auth')
 .controller('RegisterCtrl',
 ['$rootScope', '$scope', '$location', 'Auth', function($rootScope, $scope, $location, Auth) {
-    $scope.role = routingConfig.userRoles.user;
+    $scope.role = Auth.userRoles.user;
+    $scope.userRoles = Auth.userRoles;
 
     $scope.register = function() {
         Auth.register({
