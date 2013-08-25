37fb07ebadef67f25283809b897d807561a5e0d4
aozkan <armagan.ozkan@gmail.com>
2013-08-24 20:42:54 -0400

    Update controllers.js
    Inside the RegisterCtrl changed $scope.user = Auth.userRoles.user;
    to Auth.user;  I did not see userRoles with a user property.  I could be wrong please have a look.
    
    Thanks.

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -50,7 +50,7 @@ angular.module('angular-client-side-auth')
 angular.module('angular-client-side-auth')
 .controller('RegisterCtrl',
 ['$rootScope', '$scope', '$location', 'Auth', function($rootScope, $scope, $location, Auth) {
-    $scope.role = Auth.userRoles.user;
+    $scope.role = Auth.user;
     $scope.userRoles = Auth.userRoles;
 
     $scope.register = function() {
