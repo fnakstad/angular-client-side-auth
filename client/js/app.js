0dcaf30ab058d76b60777bbe34f177e338f9b2fc
Frederik Nakstad <nakstad@gmail.com>
2013-12-16 15:19:09 +0900

    Switched out ngRoute with ui-router

--- client/js/app.js
+++ client/js/app.js
@@ -1,48 +1,50 @@
 'use strict';
 
-angular.module('angular-client-side-auth', ['ngCookies', 'ngRoute'])
+angular.module('angular-client-side-auth', ['ngCookies', 'ui.router'])
 
-    .config(['$routeProvider', '$locationProvider', '$httpProvider', function ($routeProvider, $locationProvider, $httpProvider) {
+    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
 
     var access = routingConfig.accessLevels;
 
-    $routeProvider.when('/',
-        {
-            templateUrl:    'home',
-            controller:     'HomeCtrl',
-            access:         access.user
+    $stateProvider
+        .state('Home', {
+            url: '/',
+            templateUrl: 'home',
+            controller: 'HomeCtrl',
+            access: access.user
+        })
+        .state('Login', {
+            url: '/login',
+            templateUrl: 'login',
+            controller: 'LoginCtrl',
+            access: access.anon
+        })
+        .state('Register', {
+            url: '/register',
+            templateUrl: 'register',
+            controller: 'RegisterCtrl',
+            access: access.anon
+        })
+        .state('Private', {
+            url: '/private',
+            templateUrl: 'private',
+            controller: 'PrivateCtrl',
+            access: access.user
+        })
+        .state('Admin', {
+            url: '/admin',
+            templateUrl: 'admin',
+            controller: 'AdminCtrl',
+            access: access.admin
+        })
+        .state('404', {
+            url: '/404',
+            templateUrl: '404',
+            access: access.public
         });
-    $routeProvider.when('/login',
-        {
-            templateUrl:    'login',
-            controller:     'LoginCtrl',
-            access:         access.anon
-        });
-    $routeProvider.when('/register',
-        {
-            templateUrl:    'register',
-            controller:     'RegisterCtrl',
-            access:         access.anon
-        });
-    $routeProvider.when('/private',
-        {
-            templateUrl:    'private',
-            controller:     'PrivateCtrl',
-            access:         access.user
-        });
-    $routeProvider.when('/admin',
-        {
-            templateUrl:    'admin',
-            controller:     'AdminCtrl',
-            access:         access.admin
-        });
-    $routeProvider.when('/404',
-        {
-            templateUrl:    '404',
-            access:         access.public
-        });
-    $routeProvider.otherwise({redirectTo:'/404'});
 
+
+    $urlRouterProvider.otherwise('/404');
     $locationProvider.html5Mode(true);
 
     $httpProvider.interceptors.push(function($q, $location) {
@@ -61,9 +63,9 @@ angular.module('angular-client-side-auth', ['ngCookies', 'ngRoute'])
 
 }])
 
-    .run(['$rootScope', '$location', '$http', 'Auth', function ($rootScope, $location, $http, Auth) {
+    .run(['$rootScope', '$location', 'Auth', function ($rootScope, $location, Auth) {
 
-        $rootScope.$on("$routeChangeStart", function (event, next, current) {
+        $rootScope.$on("$stateChangeStart", function (event, next, current) {
             $rootScope.error = null;
             if (!Auth.authorize(next.access)) {
                 if(Auth.isLoggedIn()) $location.path('/');
