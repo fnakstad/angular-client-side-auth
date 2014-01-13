7ba4ea612ee0be20356e91e6622af17d28a20207
Frederik Nakstad <nakstad@gmail.com>
2014-01-13 17:59:20 +0900

    Removed the useless title property from the accessLevels object.

--- client/js/routingConfig.js
+++ client/js/routingConfig.js
@@ -69,8 +69,7 @@
                     }
                     //accessLevels[level] = parseInt(resultBitMask, 2);
                     accessLevels[level] = {
-                        bitMask: parseInt(resultBitMask, 2),
-                        title: accessLevelDeclarations[level]
+                        bitMask: parseInt(resultBitMask, 2)
                     };
                 }
                 else console.log("Access Control Error: Could not parse '" + accessLevelDeclarations[level] + "' as access definition for level '" + level + "'")
@@ -85,8 +84,7 @@
                     else console.log("Access Control Error: Could not find role '" + accessLevelDeclarations[level][role] + "' in registered roles while building access for '" + level + "'")
                 }
                 accessLevels[level] = {
-                    bitMask: resultBitMask,
-                    title: accessLevelDeclarations[level][role]
+                    bitMask: resultBitMask
                 };
             }
         }
