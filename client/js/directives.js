aa445ee83e0c5ad6552a72bc196c6b2bf66a1619
danteata <danteata@gmail.com>
2014-02-10 13:21:09 +0000

    use ui-routers ui-sref and ui-sref-active instead of activeNav directive

--- client/js/directives.js
+++ client/js/directives.js
@@ -33,35 +33,3 @@ angular.module('angular-client-side-auth')
     };
 }]);
 
-angular.module('angular-client-side-auth').directive('activeNav', ['$location', function($location) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var anchor = element[0];
-            if(element[0].tagName.toUpperCase() != 'A')
-                anchor = element.find('a')[0];
-            var path = anchor.href;
-
-            scope.location = $location;
-            scope.$watch('location.absUrl()', function(newPath) {
-                path = normalizeUrl(path);
-                newPath = normalizeUrl(newPath);
-
-                if(path === newPath ||
-                    (attrs.activeNav === 'nestedTop' && newPath.indexOf(path) === 0)) {
-                    element.addClass('active');
-                } else {
-                    element.removeClass('active');
-                }
-            });
-        }
-
-    };
-
-    function normalizeUrl(url) {
-        if(url[url.length - 1] !== '/')
-            url = url + '/';
-        return url;
-    }
-
-}]);
