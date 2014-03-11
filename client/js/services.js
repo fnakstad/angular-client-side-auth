3d877a67889edc3d72a098e888b7d78921614771
Redian <redian.ibra@gmail.com>
2014-03-11 11:51:09 +0000

    Linting Fixes.
    A few linting fixes and i would suggest using the identity operator === instead of the equality operator.

--- client/js/services.js
+++ client/js/services.js
@@ -11,19 +11,21 @@ angular.module('angular-client-side-auth')
 
     function changeUser(user) {
         angular.extend(currentUser, user);
-    };
+    }
 
     return {
         authorize: function(accessLevel, role) {
-            if(role === undefined)
+            if(role === undefined) {
                 role = currentUser.role;
+            }
 
             return accessLevel.bitMask & role.bitMask;
         },
         isLoggedIn: function(user) {
-            if(user === undefined)
+            if(user === undefined) {
                 user = currentUser;
-            return user.role.title == userRoles.user.title || user.role.title == userRoles.admin.title;
+            }
+            return user.role.title === userRoles.user.title || user.role.title === userRoles.admin.title;
         },
         register: function(user, success, error) {
             $http.post('/register', user).success(function(res) {
