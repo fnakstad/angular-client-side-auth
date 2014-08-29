fdb5050586bd3aba593eeb4a11abd77d7c31fa3b
Patrick Costa <patrickrbcosta@gmail.com>
2014-08-28 22:58:14 -0300

    Update bodyparser middleware

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var app = module.exports = express();
 app.set('views', __dirname + '/client/views');
 app.set('view engine', 'jade');
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'client')));
